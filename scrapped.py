e6431ad9ea9c5147802c4c622e7d96f207d236b4
nameless-86 <porongo46@hotmail.com>
2022-06-15 14:40:15 -0300

    Pasando a dict

--- scrapped.py
+++ scrapped.py
@@ -1,25 +1,33 @@
 ##Llamo a la libreria csv para poder manipular listings.csv
 import csv
 #Variables para contar o llenar
-dias = []
+dicti = {}
+dias= []
+
 counter = 0
 
 #abro el archivo
 with open('listings.csv') as csvfile:
-    archivo = csv.reader(csvfile)
+    archivo = csv.reader(csvfile, delimiter=',')
     for i in archivo:
-            dias.append(i[15])
-#Hago una lista con la columna de los dias disponibles al año y le saco el titulo
+        #Esto tendria que leer el titulo de la columna y pararse dependiendo lo que se le pida 
+        #TERMINAR
+        dicti[i[1]]=i[15]
 
-dias.pop(0)
+print(dicti)
+
 #paso los valores de str a ints
-for j in range(0, len(dias)):
-    dias[j] = int(dias[j])
+for j in dicti:
+    #hay que sacar el primer elemento de la columna porque es un string con una palabra y no pasa a entero
+    print(dicti[j])
+
+#Falta contar por cada barrio o tipo de habitacion
+
 #Itero por todos los dias de la lista, si alguno es igual a 365 lo cuento
-for k in dias:
-    if k == 365:
-        print(k)
-        counter += 1
+# for k in dias:
+#     if k == 365:
+#         print(k)
+#         counter += 1
         
-print(dias)
+# print(dias)
 print("De todos los alojamientos", counter, "Estan disponibles todo el año")
